62652c71c6cb572070faa405a22205553507a1a5
JAY DAVE <jaydave8032005@outlook.com>
2024-12-10 21:50:47 +0530

    Add project files.

---
+++ Controllers/CityController.cs
@@ -0,0 +1,33 @@
+﻿using ConsumeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeAPI.Controllers
+{
+    public class CityController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7104/api/City");
+        private readonly HttpClient _client;
+        public CityController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GET()
+        {
+            List<CityModel> city = new List<CityModel>();
+            HttpResponseMessage response = _client.GetAsync(baseAddress).Result;
+            if (response .IsSuccessStatusCode) 
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                city = JsonConvert.DeserializeObject<List<CityModel>>(data);
+            }
+            return View("CityList", city);
+        }
+    }
+}
---
+++ Models/CityModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsumeAPI.Models
+{
+    public class CityModel
+    {
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+        public string StateName { get; set; }
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        public string CityName { get; set; }
+        public string PinCode { get; set; }
+    }
+}
